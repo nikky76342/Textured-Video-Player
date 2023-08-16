112986735a7c31e19fb82c31e27e8860f910a449
nikhilreddy <nikhilreddy5478@gmail.com>
2023-08-16 15:14:19 +0530

    Initial Commit

--- app/src/main/java/com/nikhil/texturedvideoplayer/MyTestActivity.kt
+++ app/src/main/java/com/nikhil/texturedvideoplayer/MyTestActivity.kt
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MyTestActivity : AppCompatActivity() {
 
-    private val video_url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
-private lateinit var customTextureView: CustomTextureView
+    private val video_url =
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
+    private lateinit var customTextureView: CustomTextureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mytest)
-        customTextureView = findViewById<CustomTextureView>(R.id.ctvnik)
-        customTextureView.setVideoUri(this, video_url)
+        customTextureView = findViewById(R.id.customTextureView)
+        customTextureView.setVideoUri(
+            this,
+            video_url,
+            object : CustomTextureView.OnVideoPrepareListener {
+                override fun onVideoPrepared() {
+                    customTextureView.playVideo()
+                }
+            })
     }
 }
